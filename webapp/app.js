298cd980b579598dc08e9b9f696245abc51ca31c
Luca Fabbri <l.fabbri@bopen.eu>
2022-10-19 18:42:08 +0200

    Do ass invalid state on siblings

--- webapp/app.js
+++ webapp/app.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
-  const $config = $('#configuration');
-  const $constraints = $('#constraints');
-  const $form = $('#form');
-  const $disabled = $('#disabled');
+  const $config = $("#configuration");
+  const $constraints = $("#constraints");
+  const $form = $("#form");
+  const $disabled = $("#disabled");
 
   if (localStorage.getItem("config") !== null) {
     $config.val(localStorage.getItem("config"));
@@ -15,67 +15,68 @@ $(document).ready(() => {
   let config = JSON.parse($config.val());
   let constraints = JSON.parse($constraints.val());
 
-  $constraints.on('change', () => {
+  $constraints.on("change", () => {
     constraints = JSON.parse(ev.target.value);
-    localStorage.setItem('constraints', ev.target.value);
+    localStorage.setItem("constraints", ev.target.value);
   });
 
-  $config.on('change', (ev) => {
-    console.log('ssss');
+  $config.on("change", (ev) => {
+    console.log("ssss");
     config = JSON.parse(ev.target.value);
-    localStorage.setItem('config', ev.target.value);
+    localStorage.setItem("config", ev.target.value);
     prepareForm();
   });
 
   const getSelection = () => {
     const selected = {};
-    $form.find('input:checked').each((i, el) => {
-      selected[el.name] = selected[el.name] ? [...selected[el.name], el.value]: [el.value];
-      console.log(selected)
+    $form.find("input:checked").each((i, el) => {
+      selected[el.name] = selected[el.name]
+        ? [...selected[el.name], el.value]
+        : [el.value];
+      console.log(selected);
     });
     return selected;
-  }
+  };
 
   const validate = async (constraints, selection) => {
     const formData = new FormData();
-    formData.append('constraints', JSON.stringify(constraints));
-    formData.append('selection', JSON.stringify(selection));
+    formData.append("constraints", JSON.stringify(constraints));
+    formData.append("selection", JSON.stringify(selection));
     try {
-      result = fetch('http://localhost:8086/validate', {
-        method: 'POST',
-        cache: 'no-cache',
+      result = fetch("http://localhost:8086/validate", {
+        method: "POST",
+        cache: "no-cache",
         body: formData,
       }).then((response) => response.json());
-
     } catch (err) {
-      console.log(err)
+      console.log(err);
       return Promise.reject();
     }
     return Promise.resolve(result);
-  }
+  };
 
-  const updateValidityState = (state) => {
+  const updateValidityState = (state, fieldName = null) => {
     Object.entries(state).forEach(([name, validValues]) => {
       const $input = $(`input[name="${name}"]`);
       $input.each((i, el) => {
-        const $field = $(el).closest('.field');
+        const $field = $(el).closest(".field");
         if (validValues.includes(el.value)) {
-          $field.get(0).classList.remove('invalid');
-        } else {
-          $field.get(0).classList.add('invalid');
+          $field.get(0).classList.remove("invalid");
+        } else if (el.name !== fieldName) {
+          $field.get(0).classList.add("invalid");
         }
       });
     });
-  }
+  };
 
   async function inputChange(ev) {
     const $this = $(this);
     console.log($this.val());
-    $disabled.css({ top: 0, bottom: 0, width: '100%'});
+    $disabled.css({ top: 0, bottom: 0, width: "100%" });
     const result = await validate(constraints, getSelection());
-    $disabled.css({ top: '', bottom: '', width: ''});
+    $disabled.css({ top: "", bottom: "", width: "" });
     console.log(result);
-    updateValidityState(result);
+    updateValidityState(result, ev.target.name);
   }
 
   const prepareForm = () => {
@@ -83,14 +84,15 @@ $(document).ready(() => {
     Object.entries(config).forEach(([name, values]) => {
       const newFieldSet = $(`<fieldset><legend>${name}</legend></fieldset>`);
       values.forEach((value) => {
-        const newField = $(`<label class="field"><input type="checkbox" name="${name}" value="${value}"> ${value}</label>`);
-        const newInput = $('input', newField);
-        newInput.on('change', inputChange);
+        const newField = $(
+          `<label class="field"><input type="checkbox" name="${name}" value="${value}"> ${value}</label>`
+        );
+        const newInput = $("input", newField);
+        newInput.on("change", inputChange);
         newField.appendTo(newFieldSet);
       });
       $form.append(newFieldSet);
     });
-  }
+  };
   prepareForm();
-
 });
