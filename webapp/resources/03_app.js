9a6e25682a32aeeecf03303c420153162fda20a4
Luca Fabbri <l.fabbri@bopen.eu>
2022-10-24 18:47:59 +0200

    5 different demos

---
+++ webapp/resources/03_app.js
@@ -0,0 +1,14 @@
+window.updateValidityState = (state, config, fieldName = null) => {
+  Object.entries(config).forEach(([name, values]) => {
+    const $input = $(`input[name="${name}"]`);
+    $input.each((i, el) => {
+      const validValues = state[el.name] || [];
+      const $field = $(el).closest(".field");
+      if (!validValues.includes(el.value)) {
+        $field.get(0).classList.add("invalid");
+      } else {
+        $field.get(0).classList.remove("invalid");
+      }
+    });
+  });
+};
