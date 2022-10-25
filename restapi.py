805ecfe51cdb25147ae4b3a6f984aa6ea572bce6
Nicola Masotti <nicolamasotti@hotmail.it>
2022-10-25 12:05:38 +0200

    update import statement

--- restapi.py
+++ restapi.py
@@ -1,11 +1,11 @@
 import json
-from constrictor import apply_constraints, load_combinations
+from request_constraints import constrictor
 from tranquilizer import tranquilize
 
 
 @tranquilize(method="post")
 def validate(constraints: str, selection: str) -> str:
-    parsed_constraints = load_combinations(json.loads(constraints))
+    parsed_constraints = constrictor.load_combinations(json.loads(constraints))
     parsed_selection = json.loads(selection)
-    result = apply_constraints(parsed_constraints, parsed_selection)
+    result = constrictor.apply_constraints(parsed_constraints, parsed_selection)
     return result
