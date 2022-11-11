fdf4fc65e7c66a545be459737ae10a3bb936b2c6
Nicola Masotti <nicolamasotti@hotmail.it>
2022-11-11 10:41:14 +0100

    Refactor. Package will contain only web app code. Algorithm code is now in cads_catalogue_api_service

--- restapi.py
+++ restapi.py
@@ -18,17 +18,17 @@ class Data(BaseModel):
 def validate(
     data: Data,
 ) -> dict[str, list[Any]]:
-    parsed_possible_selections = constrictor.parse_possible_selections(
+    parsed_form = constrictor.parse_form(
         json.loads(data.configuration)
     )
-    parsed_valid_combinations = constrictor.parse_valid_combinations(
+    parsed_combinations = constrictor.parse_combinations(
         json.loads(data.constraints)
     )
-    parsed_current_selection = constrictor.parse_current_selection(
+    parsed_selection = constrictor.parse_selection(
         json.loads(data.selection)
     )
     result = constrictor.apply_constraints(
-        parsed_possible_selections, parsed_valid_combinations, parsed_current_selection
+        parsed_form, parsed_combinations, parsed_selection
     )
     return result
 
